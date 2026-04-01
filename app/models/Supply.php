ee8a65da182953975865667c994d07386973fbc1
Amen Allah <amanallah.jendoubi@gmail.com>
2026-04-01 21:20:40 +0200

    setting images for electronic products

---
+++ app/models/Supply.php
@@ -0,0 +1,8 @@
+<?php
+
+
+
+
+class Supply{
+    private $quantity;
+}
---
+++ app/views/category.view.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+        theme: {
+        extend: {
+            screens: {
+            'mouse': {'raw': '(hover: hover)'},
+            }
+        }
+        }
+    }
+    </script>
+
+    <title>Cart</title>
+</head>
+<body>
+    <?php 
+        include 'header.view.php';
+    ?>
+    <main class="w-[95%] mx-auto">
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    </main>
+    <?php 
+        include 'footer.view.php';
+    ?> 
+</body>
+</html>

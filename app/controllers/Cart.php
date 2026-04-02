fc46493cc70fc02b3eff267f4ae77debcdc04c6d
Amen Allah <amanallah.jendoubi@gmail.com>
2026-04-02 18:39:48 +0200

    store cart info in session variable cart

--- app/controllers/Cart.php
+++ app/controllers/Cart.php
@@ -4,20 +4,17 @@ class Cart extends Controller{
     function index (){
         $this->view('cart');
     }
-    
-    function updateCart() {
+
+    function updateCart() { //only update the basket 
         $body = json_decode(file_get_contents('php://input'), true);
         if (!isset($_SESSION['cart']['count'])) {
             $_SESSION['cart']['count'] = 0;
         } 
-        $_SESSION['cart']['count'] += $body['quantity'];
-        
+        $_SESSION['cart']['count'] += $body['productQuantity'];
+        $_SESSION['cart'][]= ['productId'=>$body['productId'],'productQuantity'=>$body['productQuantity']];
         echo json_encode([
             'success' => true,
             'count' => $_SESSION['cart']['count']
         ]);
     }
-
-
-
 }
--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -1,19 +1,17 @@
 console.log('eklgg');
 const cartBtn = document.querySelector('.js-add-to-cart');
 cartBtn.addEventListener('click', async () => {
-    let count = 0;
     const select = document.querySelector('.js-my-select');
-    const quantity = select.value;
-    const id=document.querySelector('.');
 
-    count += parseInt(quantity); // Convert string to number
+    const productQuantity= select.value;
+    const productId = window.location.pathname.split('/').pop();
     
     const res = await fetch('http://localhost/E-Commerce-Website/public/cart/updateCart', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ quantity: count}) 
+        body: JSON.stringify({productQuantity : productQuantity, productId:productId }) 
     });
     const data = await res.json();
     const counter=document.querySelector('.js-cart-counter');
